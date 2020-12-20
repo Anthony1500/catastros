1451d12fa11ecdf5d807c65113263d84956d5d45
Anthony Arteaga <javito_12@hotmail.com>
2020-12-19 22:22:57 -0500

    avance-2

--- main.php
+++ main.php
@@ -12,7 +12,9 @@
 	<link rel="stylesheet" type="text/css" href="css/proyecto.css">
     <script type="text/javascript" src="js/jquery-easyui-1.8.8/jquery.min.js"></script>
     <script type="text/javascript" src="js/jquery-easyui-1.8.8/jquery.easyui.min.js"></script>
-    <script type="text/javascript" src="js/jquery-easyui-1.8.8/locale/easyui-lang-es.js"></script>
+	<script type="text/javascript" src="js/jquery-easyui-1.8.8/locale/easyui-lang-es.js"></script>
+    <link href="./main.css" rel="stylesheet">
+    <link href="./icon.css" rel="stylesheet">
 </head>
 <?php
  session_start();
@@ -36,52 +38,45 @@
 
         
        
-		<div data-options="region:'west',split:true" title="Menu" style="width:200px;">
-        
-        <ul class="easyui-tree">
-			<li>
-				<span>Menu</span>
-				<ul>
-					<li>
-						<span>Ingreso de datos</span>
-						<ul>
-						<li>
-						<span>Administrador</span>
-						<ul>
-						
-						
-							
-							<li>
-								   <a  href="main.php?pag=listausuario"> Ingreso credenciales </a> 
-							</li>
-							</ul>
-							</li>
-							<li>
-						<span>Administracion de usuarios</span>
-						<ul>
-							<li>
-							<a  href="main.php?pag=listabodega"> Ingreso usuarios </a>
-							</li>
-							</ul>
-							</li>
-							
-							<li>
-						<span>Reportes</span>
-						<ul>
-							<li>
-							<a  href="main.php?pag=reportes"> Reportes </a>
-							</li>
-							</ul>
-							</li>
-						</ul>
-					</li>
-				
-					</li>
-					 
-				</ul>
-			</li>
-		</ul>
-
+		<div data-options="region:'west',split:true" title="Menu" style="width: 252.583px; height: 737.233px;">
+	
+               
+                                
+		
+		
+		
+                        
+                                     
+                 
+        <div class="app-sidebar__inner">
+                            <ul class="vertical-nav-menu">
+                             <li  class="app-sidebar__heading">&nbsp;&nbsp;Administrador</li>
+                                <li>
+                                    <a  href="main.php?pag=listausuario">
+                                        <i class="metismenu-icon icon"></i>
+                                        Ingreso credenciales
+                                    </a>
+                                </li>
+                                
+                               
+                                <li class="app-sidebar__heading">&nbsp;&nbsp;Administracion</li>
+                                <li>
+                                    <a href="main.php?pag=listabodega">
+                                        <i class="metismenu-icon pe-7s-display2"></i>
+                                        Ingreso usuarios
+                                    </a>
+                                </li>
+                               
+                                <li class="app-sidebar__heading">&nbsp;&nbsp;Mapa</li>
+                                <li>
+                                    <a  href="main.php?pag=mapa" >
+                                        <i class="metismenu-icon pe-7s-graph2">
+                                        </i>
+                                        mapa
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
         </div>
         <div data-options="region:'center' ">
 		<?php
@@ -90,12 +85,12 @@
 			$page = $page.".php";
 			include ($page);
 		}	
-		
+		//iconos en esta web: https://icomoon.io/app/#/select
 			?>
 	
 	
         </div>
  
- 
+		<script type="text/javascript" src="./assets/scripts/main.js"></script>
 </body>
 </html>
